3e9f2b7c7e94b1a575c171f258b09f836d6aba4c
Karla <Karla@DESKTOP-D45101L>
2023-01-03 21:17:17 +0100

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fil = new OpenFileDialog();
+            fil.ShowDialog();
+            string path = fil.FileName.ToString();
+            ExcelFileReader(path);
+        }
+
+        public void ExcelFileReader(string path)
+        {
+            var stream = File.Open(path, FileMode.Open, FileAccess.Read);
+            var reader = ExcelReaderFactory.CreateReader(stream);
+            var result = reader.AsDataSet();
+            var tables = result.Tables.Cast<DataTable>();
+
+            foreach(DataTable table in tables)
+            {
+                dataGridView1.DataSource = table;
+            }
+        }
+    }
+}
